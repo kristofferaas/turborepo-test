4ad47c03754766565bae8823f65fbc074d679d3b
Kristoffer Aas <mail@kristoffer.io>
2021-12-19 20:55:58 +0100

    Added next.js mulitzones

---
+++ apps/www/next.config.js
@@ -0,0 +1,22 @@
+const withTM = require("next-transpile-modules")(["ui"]);
+const { DOCS_URL } = process.env;
+
+module.exports = withTM({
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      {
+        source: "/docs",
+        destination: `${DOCS_URL}/docs`,
+      },
+      {
+        source: "/docs/:path*",
+        destination: `${DOCS_URL}/docs/:path*`,
+      },
+    ];
+  },
+});
